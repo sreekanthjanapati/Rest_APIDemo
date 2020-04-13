690a624cb25af139bd08cfd5da7e57f4bf9d66de
SRJANA <SRJANA@APINP-ELPT60235.mshome.net>
2020-04-13 17:01:41 +0530

    Comitting the file into git for practice

---
+++ src/test/java/com/demo/api/Rest_APIDemo/RestApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.api.Rest_APIDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
